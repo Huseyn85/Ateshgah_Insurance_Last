e6d9348d4a3fe256c66e11f744f5dd63c01fcd44
Programmingunec <azwebacademy@gmail.com>
2020-09-28 22:09:35 +0300

    Atesgah

---
+++ js/app.js
@@ -0,0 +1,106 @@
+// Dark Mode
+
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
+
+$(document).ready(function(){
+  $(".light_box1").click(function(){
+    $(".light_box1").addClass("active_light");
+    $(".light_box2").removeClass("active_light");
+  });
+
+  $(".light_box2").click(function(){
+    $(".light_box2").addClass("active_light");
+    $(".light_box1").removeClass("active_light");
+  });
+});
+
+
+
+
+// Fullwidth Screen
+
+function fullWidth(){
+  document.getElementById("fullScreen_left").style.opacity = "0"
+  document.getElementById("fullScreen_right").style.width = "100%"
+}
+
+
+
+// Sidenav - Media
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+// Clickable Dropdown
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+
+
+
+// For TAB MENU
+
+function openTab(evt, topicName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(topicName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// Tab2
+
+function openTopic(evt, topicName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(topicName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+
